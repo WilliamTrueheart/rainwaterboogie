8f6cb81111f2fc266b689e10647ec5cdf8b6681a
Will Newton <91620451+WilliamTrueheart@users.noreply.github.com>
2022-05-12 00:14:39 -0500

    Add files via upload

--- js.js
+++ js.js
@@ -1,8 +1,12 @@
 const expand_Project = (num) => {
     let projectTitle1 = document.getElementById('projectTitle_1');
+    let projectTitle2 = document.getElementById('projectTitle_2');
+    let projectDesc2 = document.getElementById('projectDesc_2');
     let projectDesc1 = document.getElementById('projectDesc_1');
     let projectDisplay_1 = document.getElementById('projectDisplay_1');
+    let projectDisplay_2 = document.getElementById('projectDisplay_2');
     let returnFromProjects = document.getElementById('returnFromProjects');
+
 
     returnFromProjects.style.display = 'flex';
     if (num == 1) {
@@ -10,16 +14,28 @@ const expand_Project = (num) => {
     projectTitle1.innerText = 'Fizz Buzz'
     projectDesc1.innerText = "Let's fire up the fizz buzz machine!"
     }
+    if (num == 2) {
+        projectDisplay_2.classList.add('projectDisplayActive');
+        projectTitle2.innerText = 'Mad Lib'
+        projectDesc2.innerText = "Will mad libs ever be 'uncool'? No..."  
+    }
 }
 
 const returnFromProjects = (num) => {
     let projectDisplay_1 = document.getElementById('projectDisplay_1');
+    let projectDisplay_2 = document.getElementById('projectDisplay_2');
     let outputFizzBuzz = document.getElementById('outputFizzBuzz');
     let returnFromProjects = document.getElementById('returnFromProjects');
+    let madLibResults = document.getElementById('madLibResults');
+    let madLib = document.getElementById('madLib');
 
     projectDisplay_1.classList.remove('projectDisplayActive');
+    projectDisplay_2.classList.remove('projectDisplayActive');
     returnFromProjects.style.display = 'none';    
     outputFizzBuzz.innerText = '';
+    madLibResults.innerText = '';
+    madLib.style.display = 'flex';
+    madLibResults.style.display = 'none';
 }
 
 
@@ -52,4 +68,24 @@ const calculateFizzBuzz = () => {
         }
         outputFizzBuzz.innerText = fizzBuzz;
     }
+}
+
+const composeMadLib = () => {
+    let madLib = document.getElementById('madLib');
+    let madLibResults = document.getElementById('madLibResults');
+    let noun1 = document.getElementById('noun1').value;
+    let noun2 = document.getElementById('noun2').value;
+    let name1 = document.getElementById('name1').value;
+    let adj1 = document.getElementById('adj1').value;
+    let verb1 = document.getElementById('verb1').value;
+    let verb2 = document.getElementById('verb2').value;
+
+    madLib.style.display = 'none';
+    madLibResults.style.display = 'flex';
+    
+    madLibResults.innerText = 'Long, long ago, there was a ' + noun1 +
+    ' that went by the name of ' + name1 + '. Despite being so ' + adj1 + ', ' +
+    name1 + ' always seemed to be ' + verb1 + '. When asked, the common explanation is that, '
+    + ' "well, If theres a ' + noun2 + ' out there ' + verb2 + ' , then I will always be '
+    + verb1 + '!'; 
 }
